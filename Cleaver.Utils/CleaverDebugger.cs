519eeb3357e7c22a610261d60aadc95b8cf94ca1
chungvodim <ba198647@gmail.com>
2016-01-14 19:40:25 +0800

    up

--- Cleaver.Utils/CleaverDebugger.cs
+++ Cleaver.Utils/CleaverDebugger.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-//using System.Data.Entity.Validation;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +13,7 @@ namespace Cleaver.Utils
     public class CleaverDebugger
     {
         private static Logger _logger = LogManager.GetCurrentClassLogger();
-        //private static TextWriterTraceListener myListener = new TextWriterTraceListener("Info.svclog");
-        public CleaverDebugger()
-        {
-            //System.Diagnostics.Debug.Listeners.Add(myListener);         
-            //System.Diagnostics.Trace.Listeners.Add(myListener);
-            //System.Diagnostics.Debug.WriteLine("Error");
-            //System.Diagnostics.Debug.Flush();
-            //t.WriteLine(DateTime.Now.ToString() + " > " + "Error");
-            //t.Flush();
-        }
+
         public static void Info(object obj)
         {
             _logger.Info(obj);
@@ -30,59 +21,36 @@ namespace Cleaver.Utils
         public static void Info(string msg)
         {
             //String path = Directory.GetCurrentDirectory();
-            DateTime datetime = DateTime.Now;
-            String fileName = "Cleaver_" + datetime.ToString("dd_MM_yyyy") + ".Log";
-            File.AppendAllText(fileName, datetime.ToString("MM-dd hh:mm") + " > " + msg);
-            File.AppendAllText(fileName, "\n");
-            //myListener.WriteLine("Sending trace information");
-            //Trace.Flush();
-            //myListener.Flush();
+            Log(msg, "[INFO]");
         }
-
         public static void Error(string msg)
         {
-            _logger.Error(msg);
+            Log(msg,"[ERROR]");
         }
         public static void Error(Exception ex)
         {
-            _logger.Error(ex);
-            //if (ex.GetType() == typeof(DbEntityValidationException))
-            //{
-            //    foreach (var eve in (ex as DbEntityValidationException).EntityValidationErrors)
-            //    {
-            //        _logger.Error("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:", eve.Entry.Entity.GetType().Name, eve.Entry.State);
-            //        foreach (var ve in eve.ValidationErrors)
-            //        {
-            //            _logger.Error("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage);
-            //        }
-            //    }
-            //}
-            //else
-            //{
-            //    _logger.Error(ex);
-            //}
+            if (ex.GetType() == typeof(DbEntityValidationException))
+            {
+                foreach (var eve in (ex as DbEntityValidationException).EntityValidationErrors)
+                {
+                    Error(string.Format("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:", eve.Entry.Entity.GetType().Name, eve.Entry.State));
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Error(string.Format("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
+                    }
+                }
+            }
+            else
+            {
+                Error(ex.Message);
+            }
         }
-        //public static void Log(String message)
-        //{
-        //    DateTime datetime = DateTime.Now;
-        //    String fileName = "Cleaver_" + datetime.ToString("dd_MM_yyyy") + ".Log";
-        //    if (!File.Exists(fileName))
-        //    {
-        //        System.IO.FileStream f = File.Create(fileName);
-        //        f.Close();
-        //    }
-
-        //    try
-        //    {
-        //        System.IO.StreamWriter writter = File.AppendText(fileName);
-        //        writter.WriteLine(datetime.ToString("MM-dd hh:mm") + " > " + message);
-        //        writter.Flush();
-        //        writter.Close();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine(e.Message.ToString());
-        //    }
-        //}
+        private static void Log(string msg, string prefix)
+        {
+            DateTime datetime = DateTime.Now;
+            String fileName = "Cleaver_" + datetime.ToString("dd_MM_yyyy") + ".Log";
+            File.AppendAllText(fileName, prefix + datetime.ToString("MM-dd hh:mm") + " > " + msg);
+            File.AppendAllText(fileName, "\n");
+        }
     }
 }
